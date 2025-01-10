3633fcca9bb165e5a8764f833cc556571a6bdabc
ritikdevelop <hrithikrana216@gmail.com>
2025-01-10 14:43:01 +0530

    Update package dependencies to include lucide-react and react-native-safe-area-view
    - Added lucide-react version 0.469.0 to package.json and package-lock.json.
    - Included react-native-safe-area-view version 2.0.0, noting its deprecation in favor of react-native-safe-area-context.

---
+++ constants/Colors.ts
@@ -0,0 +1,24 @@
+const tintColorLight = "#07a367";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  WHITE: "#fff",
+  PRIMARY: "#37c468",
+  GRAY: "#7d7d7d",
+  light: {
+    text: "#074a2d",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#000",
+    tint: tintColorDark,
+    icon: "#fff",
+    tabIconDefault: "#fff",
+    tabIconSelected: tintColorDark,
+  },
+};
