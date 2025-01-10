63a520a4a2d9f3c4d56690331e2b2b6c93fa4d91
ritikdevelop <hrithikrana216@gmail.com>
2025-01-10 14:43:32 +0530

    Enhance RootLayout by adding a tab screen for improved navigation structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
 import { Stack } from "expo-router";
 import "./globals.css";
 
-
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
 }
