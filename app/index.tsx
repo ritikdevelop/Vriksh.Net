6e9fd3f336c93283814655822e282414d85ade45
ritikdevelop <hrithikrana216@gmail.com>
2025-01-10 12:56:02 +0530

    Refactor index.tsx to enhance layout and styling with Tailwind CSS classes

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="text-red-500 text-3xl">Hello</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-500 text-3xl text-center">Hello</Text>
     </View>
   );
 }
