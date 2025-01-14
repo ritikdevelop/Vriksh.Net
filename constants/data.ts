e2bfff552f5f50cc570354f1890819e18f839e8c
ritikdevelop <hrithikrana216@gmail.com>
2025-01-14 23:30:12 +0530

    Add settings configuration with icons for user profile options

---
+++ constants/data.ts
@@ -0,0 +1,28 @@
+import icons from "./icons";
+
+export const settings = [
+  {
+    title: "Profile",
+    icon: icons.person,
+  },
+  {
+    title: "Notifications",
+    icon: icons.bell,
+  },
+  {
+    title: "Security",
+    icon: icons.shield,
+  },
+  {
+    title: "Language",
+    icon: icons.language,
+  },
+  {
+    title: "Help Center",
+    icon: icons.info,
+  },
+  {
+    title: "Invite Friends",
+    icon: icons.people,
+  },
+];
